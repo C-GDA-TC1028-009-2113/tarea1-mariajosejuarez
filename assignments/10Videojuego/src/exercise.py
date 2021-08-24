7fbd121b9a573367dbc610f2df87ef36972a8a58
mariajosejuarez <a01641455@itesm.mx>
2021-08-24 04:46:32 +0000

    Tarea videojuegos

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    #A01641455
+    #María José Juárez Fernández Villanueva
+    nuevos=int(input("Dame la cantidad de juegos nuevos: "))
+    usados=int(input("Dame la cantidad de juegos usados: "))
+    cantidad=int(nuevos*1000+usados*350)
+    print("El total de la compra es:", cantidad)
     pass
 
 
