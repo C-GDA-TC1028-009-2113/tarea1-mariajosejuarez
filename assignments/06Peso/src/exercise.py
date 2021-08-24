536269702e5f714d80fcd9a5dd97a13123004600
mariajosejuarez <a01641455@itesm.mx>
2021-08-24 04:39:03 +0000

    Tarea calcular peso

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
+    pesoinicial=float(input("Dame el peso inicial: "))
+    pesofinal=float(input("Dame el peso final: "))
+    meses=int(input("Dame la cantidad de meses: "))
+    peso=float((pesoinicial-pesofinal)/meses)
+    print("Lo que debes bajar por mes es:", peso)
+
     pass
 
 
